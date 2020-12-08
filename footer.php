b96b95e188bb1db53a276b20f00aaf38f5141ea8
Emmy <60683173+emmy-html@users.noreply.github.com>
2020-12-07 20:38:12 -0600

    Changed format & style for footer logo and updated contact information

--- footer.php
+++ footer.php
@@ -1,18 +1,25 @@
 </div>
 <footer id="footer">
 <article class="text-logo">
-        <h5>The Meals On Wheels Foundation
-          <span>of Northern Illinois</span></h5>
+        <h5>Community Nutrition Network
+          <span>& Senior Services Association</span></h5>
+          <p>Has been providing meals to older adults for more than 35 years. This experience is what allows us to adapt to the ever changing landscape.</p>
       </article>
       <div class="content-wrapper">
       <article>
         <h6>Address</h6>
+          <p><b>Administrative Office</b><br>
+          Community Nutrition Network<br>
+          and Senior Services Association</p>
           <p>7222 Cermak Road<br>
-          North Riverside, IL 60546</p>
+            Suite 302<br>
+            North Riverside, IL 60546</p>
+            <h6>Email</h6>
+            <p><a href="mailto:information@cnnssa.org">information@cnnssa.org</a></p>
         <h6>Phone</h6>
-          <p>1 (312) 967-4666</p>
+          <p>1 (312) 207-5290</p>
         <h6>Fax</h6>
-          <p>1(312) 441-0641</p>
+          <p>1 (312) 441-0641</p>
         <h6>Find Us Here</h6>
           <p class="social-media">
             <a href="https://www.facebook.com/mowf/" target="_blank"><i class="fab fa-facebook-square fa-2x"></i></a>
@@ -34,7 +41,7 @@
         </article>
         <article class="image-footer-logo">
           <div class="content-wrapper">
-            <img src="<?php bloginfo('template_url'); ?>/img/MOW_america_member.png" alt="Meals On Wheels America Member 2020" />
+            <img src="<?php echo get_stylesheet_directory_uri();?>/img/footer-logo-lime.png" alt="Community Nutrition Network Logo in Lime Green" />
           </div>
         </article>
       </div>
