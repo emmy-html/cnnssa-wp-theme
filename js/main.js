93ed2bbb44c334efefdc2991ac3c600edd3cf832
Emmy <60683173+emmy-html@users.noreply.github.com>
2020-12-21 06:26:50 -0600

    Changed order of scripts and added window.onload function that stops console errors causing buttons not to work

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        // toggle between adding and removing the "active" class, to highlight the button that controls the panel
-        this.classList.toggle("active");
-        // toggle between hiding and showing the active panel
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    });
-}
 function openCity(evt, cityName) {
     // Declare all variables
     var i, tabcontent, tablinks;
@@ -51,4 +35,23 @@ function closeTopbar() {
     var suggestionPopover = document.getElementById("suggestion-box-popover");
     suggestionPopover.classList.add("hide-topbar");
   }
-  document.getElementById("defaultTab").click();
+  window.onload = function() {
+    // wait for window to load to click tab by default & activate accordion buttons on donate page
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+  
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            // toggle between adding and removing the "active" class, to highlight the button that controls the panel
+            this.classList.toggle("active");
+            // toggle between hiding and showing the active panel
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
+    document.getElementById("defaultTab").click();
+  }
